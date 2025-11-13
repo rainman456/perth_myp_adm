c390400b78fc991ba3833939fc5125f3a3a201d6
rainman456 <james@DESKTOP-H3EJQ3J.>
2025-11-13 03:20:38 +0100

    update7

--- src/index.ts
+++ src/index.ts
@@ -106,7 +106,6 @@ app.use(
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Admin');
   if (req.method === 'OPTIONS') {
     res.sendStatus(204);
     return;
